5b4662f46907ab6a88976271f4b0485e58ee93f6
Supritha R S <suprithars111@gmail.com>
2021-05-02 14:30:13 +0530

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import Button from './Button'
+const Header = () => {
+    return (
+        <header className = ' header'>
+            <h1>Todo-List</h1>
+            <Button/>
+        </header>
+    )
+}
+
+export default Header
